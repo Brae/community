0b9840faa21a386b2ebdd401e32c36cd8975fc0f
Brae <brae@hatching.io>
2019-02-06 14:16:50 +0100

    Start of new Kovter bot signature

--- modules/signatures/windows/bot_kovter.py
+++ modules/signatures/windows/bot_kovter.py
@@ -11,15 +11,25 @@ class KovterBot(Signature):
     description = "Performs HTTP requests like Kovter"
     severity = 3
     categories = ["http"]
-    authors = ["Cybersprint"]
+    authors = ["Brae"]
     minimum = "2.0"
     families = ["kovter"]
 
     def on_complete(self):
-        for http in getattr(self, "get_net_http_ex", lambda: [])():
-            if re.match("/counter/\\?id=", http["uri"]):
-                self.mark_ioc("request", "%s %s://%s%s" % (
-                    http["method"], http["protocol"], http["host"], http["uri"],
-                ))
+        # 1. Check through dropped files for strange file extensions (5 or more chars?)
+        for dropped in self.get_results("dropped", []):
+            name_parts = dropped['name'].split('.')
+            self.extension = name_parts[len(name_parts)-1]
+            if re.match(r"[a-zA-Z0-9]{5,}", self.extension):
+                self.mark_ioc("file",dropped['filepath'])                
+
+        # 2. Check through registry creations (HKEY_CLASSES_ROOT\) for keys with the same name as the file extension
+        for regkey in self.check_key(pattern=r"HKEY_CLASSES_ROOT\\\." + self.extension, regex=True, actions=["regkey_written"], all=True):
+            
+
+        # 3. Check through run keys for new .bat files or .lnk files in AppData or LocalAppData
+
+            # 3.1 Identify dropped bat or lnk file
+
 
         return self.has_marks()
