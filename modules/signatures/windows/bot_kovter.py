2b1fcb16a09c6e3efbf02ba0ff41b80f95bc7d65
Brae <brae@hatching.io>
2019-02-06 14:19:15 +0100

    Deleted old signature

--- modules/signatures/windows/bot_kovter.py
+++ modules/signatures/windows/bot_kovter.py
@@ -1,16 +1,12 @@
-# Copyright (C) 2016 Justaguy @ Cybersprint B.V.
-# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
-# See the file 'docs/LICENSE' for copying permission.
-
 import re
 
 from lib.cuckoo.common.abstracts import Signature
 
 class KovterBot(Signature):
     name = "bot_kovter"
-    description = "Performs HTTP requests like Kovter"
+    description = "Exhibits behaviour characteristic of the Kovter Bot"
     severity = 3
-    categories = ["http"]
+    categories = ["bot"]
     authors = ["Brae"]
     minimum = "2.0"
     families = ["kovter"]
