ef61787c1de9efcb4f9756621542b99ae33711c0
Brae <brae@hatching.io>
2019-02-07 17:09:12 +0100

    Updated AZORult signature. Request decryption WIP, basic checkin detection functioning

--- modules/signatures/windows/infostealer_azorult.py
+++ modules/signatures/windows/infostealer_azorult.py
@@ -8,16 +8,29 @@ class InfostealerAzorultNetwork(Signature):
     severity = 3
     confidence = 2
     categories = ["infostealer"]
-    authors = ["Brae"]
+    authors = ["Brae","Alwin"]
     minimum = "2.0"
 
     user_agent =  "Mozilla/4.0 (compatible; MSIE 6.0b; Windows NT 5.1)"
+    keys = [(0x3, 0x55, 0xae), (0xfe, 0x29, 0x36), (0xd, 0xa, 0xc8)]
+
+    def decrypt_req(self, hash_str, key):
+        out_str = ""
+        for i in xrange(len(hash_str)):
+            out_str += chr(ord(hash_str[i]) ^ key[i % len(key)])
+        return out_str
 
     def on_complete(self):
-        for http in self.get_results("network", {}).get("http_ex", []):
+        for http in self.get_results("network", {}).get("http", []):
             if http['method'] == "POST" and "index.php" in http['uri']:
-                req_ua = re.search(r"User-Agent\:[A-Za-z0-9\/\ \(\)\.\;]+", http['request'])
-                if req_ua:
-                    if req_ua.group() == self.user_agent:
-                        self.mark_ioc("network", http['request'])
+                if http['user-agent'] == self.user_agent:
+                    self.mark_ioc("network", http['uri'])
+                # Extract request data and decrypt for infected user 'ID'
+                body = http['body']
+                for key in self.keys:
+                    plaintext = self.decrypt_req(body, key)
+                    if re.match(r".*([A-Z0-9]{8}-){4}", plaintext):
+                        self.mark_ioc("checkin", plaintext)
                     
+                   
+        return self.has_marks() 
